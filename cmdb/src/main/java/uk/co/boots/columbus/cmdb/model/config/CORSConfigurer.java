c1597a2929089074e2e9cd32b0b415310617b4ef
Richard Davis <rich@davisfamily.eu>
2016-11-15 18:44:48 +0000

    Added some server side configuration

--- cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/config/CORSConfigurer.java
+++ cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/config/CORSConfigurer.java
@@ -11,7 +11,11 @@ public class CORSConfigurer extends WebMvcConfigurerAdapter{
 	
 	@Value("${jwt.header}")
 	private String authHeader; 
-
+	@Value("${cors.origin.host}")
+	private String corsOriginHost;
+	@Value("${cors.origin.port}")
+	private String corsOriginPort;
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		addToRegistry("/**/*", registry);
@@ -21,7 +25,7 @@ public class CORSConfigurer extends WebMvcConfigurerAdapter{
 
 	private void addToRegistry(String patternPath, CorsRegistry registry){
 		registry.addMapping(patternPath)
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://localhost:3000", "http://" + corsOriginHost + ":" + corsOriginPort)
 		.allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
 		.allowedHeaders("Content-Type", "Accept", "X-Requested-With", "remember-me", authHeader)
 		.allowCredentials(true).maxAge(3600);		
--- cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/rest/support/CORSSupport.java
+++ cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/rest/support/CORSSupport.java
@@ -1,11 +1,19 @@
 package uk.co.boots.columbus.cmdb.model.rest.support;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 
 public class CORSSupport {
+
+	@Value("${cors.origin.host}")
+	private static String corsOriginHost;
+	@Value("${cors.origin.port}")
+	private static String corsOriginPort;
+
 	public static HttpHeaders createCORSHeaders(){
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
+		headers.add("Access-Control-Allow-Origin", "http://" + corsOriginHost + ":" + corsOriginPort);
 		headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		headers.add("Access-Control-Allow-Credentials", "true");
 		headers.add("Access-Control-Max-Age", "3600");
