e1334c7e91f9dafdfc870f2f0c482cac46637ada
Richard Davis <rich@davisfamily.eu>
2017-02-14 16:10:38 +0000

    CORS Debug Logger added

--- cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/core/config/CORSConfigurer.java
+++ cmdb/src/main/java/uk/co/boots/columbus/cmdb/model/core/config/CORSConfigurer.java
@@ -11,7 +11,8 @@ public class CORSConfigurer extends WebMvcConfigurerAdapter{
 	
 	@Value("${jwt.header}")
 	private String authHeader; 
-	@Value("${cors.origin.hosts}")
+	@Value("#{'${cors.origin.hosts}'.split(',')}")
+//	@Value("${cors.origin.hosts}")
 	private String[] corsOriginHosts;
 	
 	@Override
@@ -23,6 +24,11 @@ public class CORSConfigurer extends WebMvcConfigurerAdapter{
 
 	private void addToRegistry(String patternPath, CorsRegistry registry){
 
+		System.out.println("***** CORS ******");
+		System.out.println("Hosts[" + corsOriginHosts.length + "]:");
+		for (int i=0; i < corsOriginHosts.length; i++)
+			System.out.println(corsOriginHosts[i]);
+		System.out.println("***** CORS ******");
 		registry.addMapping(patternPath)
 		.allowedOrigins(corsOriginHosts)
 		.allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
